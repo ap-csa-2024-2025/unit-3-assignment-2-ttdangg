48c2196a31237c112be3caae0d3a0b6f996b8f02
ttdangg <ttdang1445@gmail.com>
2024-10-29 10:17:50 -0700

    starting problem 7

--- Main.java
+++ Main.java
@@ -38,18 +38,97 @@ public class Main
 
 
     // Problem 4
-    System.out.println("Enter an integer: ");
-    int divisNum = sc.nextInt();
+    // System.out.println("Enter an integer: ");
+    // int divisNum = sc.nextInt();
 
-    if (divisNum%2 == 0)
-    {
-      System.out.println("Divisible by 2!");
-    }
-    if (divisNum%3 == 0)
-    {
-      System.out.println("Divisible by 3!")
-    }
-    if (divisNum%2 == 0 && divisNum%3 == 0)
+    // if (divisNum%2 == 0 && divisNum%3 == 0)
+    // {
+    //   System.out.println("Divisible by 2!");
+    //   System.out.println("Divisible by 3!");
+    // }
+
+    // else if (divisNum%2 == 0)
+    // {
+    //   System.out.println("Divisible by 2!");
+    // }
+    // else
+    // {
+    //   System.out.println("Divisible by 3!");
+    // }
+
+
+
+
+    // Problem 5
+    // System.out.println("Please enter an integer: ");
+    // int num = sc.nextInt();
+    // if (num%2 == 1 || num%2 == -1)
+    // {
+    //   System.out.println("Odd");
+    // }
+    // else
+    // {
+    //   System.out.println("Even");
+    // }
+
+
+
+
+    // Problem 6
+    // System.out.println("Please enter a letter grade: ");
+    // String letterGrade = sc.nextLine();
+
+    // if (letterGrade.equals("A"))
+    // {
+    //   System.out.println("90-100");
+    // }
+    // else if (letterGrade.equals("B"))
+    // {
+    //   System.out.println("80-90");
+    // }
+    // else if (letterGrade.equals("C"))
+    // {
+    //   System.out.println("70-80");
+    // }
+    // else if (letterGrade.equals("D"))
+    // {
+    //   System.out.println("60-70");
+    // }
+    // else if (letterGrade.equals("F"))
+    // {
+    //   System.out.println("0-60");
+    // }
+    // else
+    // {
+    //   System.out.println("Invalid letter grade");
+    // }
+
+
+
+
+    // Problem 7
+    int first_ran_num = (int) (Math.random() * 51);
+    int second_ran_num = (int) (Math.random() * 50 ) + 51;
+    System.out.println(first_ran_num  + " " + second_ran_num + "" + "= ?" );
+
+
+
+
+
+
+
+    // Problem 8
+    // System.out.println("What in the temperature? ");
+    // int temp = sc.nextInt();
+
+    // if (temp<97 || temp>99)
+    // {
+    //   System.out.println("NOT NORMAL");
+    // }
+    // else
+    // {
+    //   System.out.println("Temperature is OK");
+    // }
 
 
 
